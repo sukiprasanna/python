62c9fc855233da0e6798436d19a234150d656621
sukiprasanna <37357304+sukiprasanna@users.noreply.github.com>
2021-06-06 18:58:31 +0530

    Create Webscraping.py

---
+++ Webscraping.py
@@ -0,0 +1,7 @@
+from lxml import html
+import requests
+response = requests.get('http://www.data.gov/')
+doc_gov = html.fromstring(response.text)
+link_gov = doc_gov.cssselect('small a')[0]
+print("Number of datasets currently listed on data.gov:")
+print(link_gov.text)
